49694983cd65d957f203572e5eb440d6a5a0a120
Maxim Danilov <danilov.ms.dev@gmail.com>
2019-03-11 00:06:28 +0500

    add(examples): stub-functions-for-testing

---
+++ stub-functions-for-testing.js
@@ -0,0 +1,33 @@
+const sinon = require('sinon');
+
+const api = {
+  printMessage: (message) => {
+    return console.log(message);
+  },
+};
+
+// stub console.log
+const consoleLogStub = sinon.stub(console, 'log');
+const testMessage = 'a test message';
+
+// call the api
+api.printMessage(testMessage);
+
+// check the api calls console.log properly
+consoleLogStub.called; // true
+consoleLogStub.withArgs(testMessage).calledOnce; // true
+
+// restore console.log
+consoleLogStub.restore();
+
+// replace printMessage with a fake function
+const printMessageStub = sinon.stub(api, 'printMessage')
+    .callsFake((message) => {
+      console.log('This message printed by the fake function:', message);
+    });
+
+// call the api
+api.printMessage('print it'); // This message printed by the fake function: print it
+
+// restore printMessage
+printMessageStub.restore();
