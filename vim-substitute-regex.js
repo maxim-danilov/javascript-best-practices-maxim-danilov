a5442a5267d168c813f4f1ce3f36a42e0cd9a275
Maxim Danilov <danilov.ms.dev@gmail.com>
2019-03-17 12:32:26 +0500

    add(examples): vim substitute regex

---
+++ vim-substitute-regex.js
@@ -0,0 +1,25 @@
+/**
+ * Vim provides the :s (substitute) command for search and replace
+ */
+
+/**
+ * Description: change each occurence in the current line
+ * Command: s///g
+ */
+
+/**
+ * Description: change each occurence in all lines
+ * Command: %s///g
+ */
+
+/**
+ * Description: change each occurence in selection
+ * Command: '<,'>s///g
+ */
+
+/**
+ * Description: wrap a key in quotes
+ * Command: s/\(\w*:\)/"\1"/g
+ * Before: callback: undefined,
+ * After: "callback:" undefined,
+*/
